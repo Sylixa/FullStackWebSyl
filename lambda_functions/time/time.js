e92bb9f875ea95205bee5f0ccff53d4102706f5e
Sylixa <61116324+Sylixa@users.noreply.github.com>
2023-09-08 13:54:47 +0700

    Changing netlify.toml

--- lambda_functions/time/time.js
+++ lambda_functions/time/time.js
@@ -1,5 +1,5 @@
 module.exports.handler = async (event, context) => {
-  const unixTime = Math.floor(Date.now() / 1001);
+  const unixTime = Math.floor(Date.now() / 1000);
   return {
     statusCode: 200,
     body: `The Unix time is ${unixTime}`,
